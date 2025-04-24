8257026390f09e5f67dc3d9b48a4fd784aa9c9c1
JonaDurante <jonathanduranteg@gmail.com>
2025-04-23 23:02:53 -0300

    first commit

---
+++ Mi primer jueguito/main/player.go
@@ -0,0 +1,24 @@
+components {
+  id: "player"
+  component: "/main/player.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"walk_down\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/Red Pirate.tilesource\"\n"
+  "}\n"
+  ""
+  position {
+    x: 219.0
+    y: 239.0
+  }
+  scale {
+    x: 1.791832
+    y: 1.944648
+    z: 0.717408
+  }
+}
